4f27af89e3cab55c6583c7af3bf1464bcbedfe83
Mohammad Najar <mmotieian@yahoo.com>
2017-08-28 15:29:17 -0500

    built in funtional interfaces

--- OCP8/src/main/java/functional/interfaces/builtIn/MainClass.java
+++ OCP8/src/main/java/functional/interfaces/builtIn/MainClass.java
@@ -1,5 +1,7 @@
 package functional.interfaces.builtIn;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MainClass {
@@ -7,12 +9,32 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		testPredicate();
+//		testPredicate();
+//		testConsumer();
+		testFunction();
+
+	}
+
+	private static void testConsumer() {
+		Consumer<Integer> doThis = t -> System.out.println("This is the output of Consumer: " + t);
+		Consumer<Integer> andThen = t -> System.out.println("That is all you need to know.");
+		doThis.andThen(andThen).accept(4);
+	}
+
+	private static void testFunction() {
+		Function<String, Integer> function = s -> {
+			try {
+				Integer i = Integer.parseInt(s);
+				return i;
+			} catch (RuntimeException e) {
+				return 0;
+			}
+		};
+		
+		System.out.println("function.apply " + function.apply("3"));
 	}
 
 	private static void testPredicate() {
-		// TODO Auto-generated method stub
-
 		Predicate<String> startsWithA = s -> s.startsWith("A");
 		Predicate<String> EndsWithZ = s -> s.endsWith("Z");
 
